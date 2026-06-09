1ab4bfc6d2b14cb2faf49f2e34b7c6a46f1390fb
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-09 14:38:35 +0600

    Improve error handling in POST request
    Refactor POST function to improve error handling for missing parameters.

--- src/app/api/dev-burn-flood/route.ts
+++ src/app/api/dev-burn-flood/route.ts
@@ -10,19 +10,16 @@ const TOKEN_ABI = [
 export async function POST(req: NextRequest) {
 
     try {
-
         const body = await req.json();
 
         const amount = body.amount;
         const count = Number(body.count);
-
         if (!amount || !count) {
             return NextResponse.json(
                 { error: "Missing params" },
                 { status: 400 }
             );
         }
-
         const provider =
             new ethers.JsonRpcProvider(RPC);
 
