d93a430904b304fe819b124130c5a43e3f208f41
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-12 19:44:01 +0600

    Refactor timer logic in TodayMessageLoop component

--- src/app/TodayMessageLoop.tsx
+++ src/app/TodayMessageLoop.tsx
@@ -43,7 +43,6 @@ export default function TodayMessageLoop({ isDarkMode, account }: Props) {
 
   const timer = setTimeout(() => {
     setVisible(false);
-
     setTimeout(() => {
       setIndex((i) => (i + 1) % slides.length);
       setVisible(true);
