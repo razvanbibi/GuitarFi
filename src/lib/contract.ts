82d5e3c45bf245c4c684a698abc86efe7cb90001
razvanbibi <mstrazvanbibi@gmail.com>
2026-06-13 00:11:56 +0600

    Play guitar tune and its burn token function is now working fine

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -381,6 +381,49 @@ export const OXTXN_TOKEN_ABI = [
     ],
     outputs: [],
   },
+
+  {
+    type: "function",
+    name: "allowance",
+    stateMutability: "view",
+    inputs: [
+      {
+        name: "owner",
+        type: "address",
+      },
+      {
+        name: "spender",
+        type: "address",
+      },
+    ],
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+      },
+    ],
+  },
+  {
+    type: "function",
+    name: "approve",
+    stateMutability: "nonpayable",
+    inputs: [
+      {
+        name: "spender",
+        type: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    outputs: [
+      {
+        name: "",
+        type: "bool",
+      },
+    ],
+  },
 ] as const;
 
 
