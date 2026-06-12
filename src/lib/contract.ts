09ccfe44a3c27e0e750b15b1a0c9059297b797d8
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-12 20:17:59 +0600

    Remove unnecessary whitespace in contract.ts

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -1,7 +1,6 @@
 // src/lib/contract.ts
 "use client";
 import { BrowserProvider, Contract, formatUnits } from "ethers";
-
 export const OXTXN_STREAK_CONTRACT =
   "0x3C9A2992B29DD130b8E678E00eEdE76b717Ee885";
 
