0b20130dfe10b1edf3160bb6de08b48049f2c0ac
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-12 20:32:13 +0600

    Remove duplicate import of OXTXN_STREAK_ABI

--- src/lib/contract.server.ts
+++ src/lib/contract.server.ts
@@ -2,7 +2,6 @@
 import { JsonRpcProvider, Contract } from "ethers";
 import { OXTXN_STREAK_CONTRACT } from "./contract.abi";
 import { OXTXN_STREAK_ABI } from "./contract.abi";
-
 const RPC_URL = "https://forno.celo.org";
 export function getReadOnlyContractServer() {
   const provider = new JsonRpcProvider(RPC_URL);
