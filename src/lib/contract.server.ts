b048c63fd52a4bb6669dd5dc4cb28e605e1b491f
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-12 20:22:12 +0600

    Remove unnecessary newline in contract.server.ts

--- src/lib/contract.server.ts
+++ src/lib/contract.server.ts
@@ -6,7 +6,6 @@ import { OXTXN_STREAK_ABI } from "./contract.abi";
 const RPC_URL = "https://forno.celo.org";
 export function getReadOnlyContractServer() {
   const provider = new JsonRpcProvider(RPC_URL);
-
   const contract = new Contract(
     OXTXN_STREAK_CONTRACT,
     OXTXN_STREAK_ABI,
