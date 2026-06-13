8683df4a220418d0ce7dee90848645cfd7bbe88b
razvanbibi <mstrazvanbibi@gmail.com>
2026-06-14 05:50:33 +0600

    Logic change for vault celo balance

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -3222,7 +3222,7 @@ active:scale-95
               </p>
 
               <p className="text-xl font-bold text-white">
-                {userVaultBalance} CELO
+                {userCeloVaultBalance} CELO
               </p>
             </div>
 
