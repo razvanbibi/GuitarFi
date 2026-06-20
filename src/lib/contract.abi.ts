aaf7b7bb6a5fd251783d3823acad6b2ce24de377
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-21 01:11:18 +0600

    Syntax issues in OXTXN_STREAK_ABI definition

--- src/lib/contract.abi.ts
+++ src/lib/contract.abi.ts
@@ -1,15 +1,15 @@
 // src/lib/contract.abi.ts
 export const OXTXN_STREAK_ABI = [
-  {
+  { 
     type: "function", 
     name: "streak",
     stateMutability: "view",
     inputs: [{ name: "user", type: "address" }],
     outputs: [{ name: "", type: "uint256" }],
-  },
+  }, 
   {
     type: "function",
-    name: "highestStreak",
+    name: "highestStreak", 
     stateMutability: "view",
     inputs: [{ name: "user", type: "address" }],
     outputs: [{ name: "", type: "uint256" }],
