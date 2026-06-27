96c9d50f56aa57b12d3df10c85e53633ae99e4ea
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-28 00:00:29 +0600

    Format code for consistency in contract.server.ts

--- src/lib/contract.server.ts
+++ src/lib/contract.server.ts
@@ -4,11 +4,11 @@ import { GUITARFI_SMART_CONTRACT } from "./contract.abi";
 import { OXTXN_STREAK_ABI } from "./contract.abi";
 const RPC_URL = "https://forno.celo.org";
 export function getReadOnlyContractServer() {
-  const provider = new JsonRpcProvider(RPC_URL);
+  const provider = new JsonRpcProvider(RPC_URL); 
   const contract = new Contract(
     GUITARFI_SMART_CONTRACT,
-    OXTXN_STREAK_ABI,
+    OXTXN_STREAK_ABI, 
     provider
   );
-  return { contract };
+  return { contract }; 
 }
