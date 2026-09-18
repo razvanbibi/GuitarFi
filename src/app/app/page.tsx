1b2e70cffa0805194072f97e4504e84177aab115
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-09-18 20:03:10 +0600

    useState hooks in page.tsx

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -100,7 +100,7 @@ export default function HomePage() {
   const [recentlyClaimed, setRecentlyClaimed] = useState(false); 
   const [toast, setToast] = useState<Toast>(null); 
   const [pendingNFT1, setpendingNFT1] = useState<bigint | null>(null); 
-  const [pendingNFT2, setpendingNFT2] = useState<bigint | null>(null);
+  const [pendingNFT2, setpendingNFT2] = useState<bigint | null>(null); 
   const [pendingNFT3, setpendingNFT3] = useState<bigint | null>(null); 
   const [pendingNFT31, setpendingNFT31] = useState<bigint | null>(null); 
   const [nftBalances, setNftBalances] =
